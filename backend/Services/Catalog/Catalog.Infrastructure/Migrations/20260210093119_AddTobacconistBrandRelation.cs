0ec224a9fa5196ae1018bb16acdd8e2c21186dbb
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2026-02-10 10:45:20 +0100

    Fix Logic For Image in Brand and add Link from Tobacconist to Brand

---
+++ backend/Services/Catalog/Catalog.Infrastructure/Migrations/20260210093119_AddTobacconistBrandRelation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTobacconistBrandRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BrandId",
+                table: "Tobacconists",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tobacconists_BrandId",
+                table: "Tobacconists",
+                column: "BrandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tobacconists_Brands_BrandId",
+                table: "Tobacconists",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tobacconists_Brands_BrandId",
+                table: "Tobacconists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tobacconists_BrandId",
+                table: "Tobacconists");
+
+            migrationBuilder.DropColumn(
+                name: "BrandId",
+                table: "Tobacconists");
+        }
+    }
+}
